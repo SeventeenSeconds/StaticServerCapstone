677262866dc413edede5899fc8b91f9e33eea1ea
Emily Parr <eparr@student.neumont.edu>
2018-12-05 23:21:29 -0700

    deleted those two files

--- Static-Server/API/serve/servingRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const servingController = require('./servingController');
-
-router.post('/serveProject', servingController.serveProject);
-
-module.exports = router;
